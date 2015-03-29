0fc58f9f20e4a395a78a3077f258d283f7c69bb9
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2015-03-29 16:44:11 +0800

    install: validate the git tree with GPG before installing anything
    This lessens the trust requirements on the service hosting the git
    repository (personal server or GitHub) and about transmission protocols
    (like unsecure git://).  This is quite needed as I don't want a
    potential attacker to be able to change my home files without me
    noticing.
    
    Also, put my public key in this git repository so that an Internet
    connection is not needed to perform the first install.

--- install.sh
+++ install.sh
@@ -3,9 +3,14 @@
 
 INSTALL_DIR="$HOME"
 
+# Find our location
+cd "$(dirname -- "$0")" || exit $?
+SOURCE_DIR="$(pwd -P)"
+
 # Recursive installation
 # Second parameter if a prefix with a dot for hidden directories
 install_rec() {
+    local DST_FILE FILENAME IGNPAT_REF IGNPAT_TEST SRC_FILE
     local SRC_DIR="$1"
     local DST_PREFIX="$2"
     local RETURN_VAL=0
@@ -60,10 +65,72 @@ install_rec() {
     return $RETURN_VAL
 }
 
-# Find our location
-cd "$(dirname -- "$0")"
-SOURCE_DIR="$(pwd -P)"
+# Check that the git history contains GPG-signed commits
+validate_gpg_gitlog() {
+    local DST_FILE KEYFP KEYID SRC_FILE
+
+    # Check that gpg is installed
+    if ! gpg --version > /dev/null
+    then
+        echo "gnupg is not installed. Skipping signature validation."
+        return 0
+    fi
+
+    # Setup our own configuration file if there is none
+    SRC_FILE="$SOURCE_DIR/dotfiles/gnupg/gpg.conf"
+    DST_FILE="$INSTALL_DIR/.gnupg/gpg.conf"
+    if ! [ -e "$DST_FILE" ]
+    then
+        mkdir -pv -m 700 "$INSTALL_DIR/.gnupg" && ln -svf "$SRC_FILE" "$DST_FILE"
+        if ! [ -e "$DST_FILE" ]
+        then
+            echo >&2 "Error: unable to create $DST_FILE"
+            return 1
+        fi
+    fi
+
+    # Retrieve key fingerprint
+    KEYFP="$(sed -n 's/^[ \t]*signingkey[ ]*=[ ]*//p' dotfiles/gitconfig | \
+        head -n1 | tr -d ' ')"
+
+    # Import the key if it is not found
+    if ! gpg --list-key "$KEYFP" > /dev/null
+    then
+        echo "Importing key $KEYFP..."
+        gpg --import dotfiles/gnupg/mygpgpubkey.asc || return $?
+        gpg --list-key "$KEYFP" > /dev/null || return $?
+    fi
+
+    # Check that the 10 last commits are GPG-signed (with good or untrusted)
+    if git --no-pager log --format='%H=%G?' --max-count 10 | grep -v '=[GU]$'
+    then
+        echo >&2 "Error: some commits are not signed"
+        return 1
+    fi
+
+    # Check that the last commit is signed with the good GPG key
+    # To do this, grab the key ID which was used and find it among the subkeys
+    # Use "git log" instead of "git verify-commit" (git>=2.1.0)
+    KEYID="$(LANG=C git log --max-count=1 --show-signature HEAD 2>&1 | \
+        sed -n 's/gpg: Signature .* key ID \([0-9A-F]\+\)/\1/p' | head -n1)"
+    if [ -z "$KEYID" ]
+    then
+        echo >&2 "Error: unable to parse the output of 'git verify-commit HEAD'"
+        return 1
+    fi
+    if ! (gpg --list-key "$KEYFP" | grep -q "^sub .*/$KEYID ")
+    then
+        echo >&2 "Error: the commit has been signed with key $KEYID not in $KEYFP"
+        return 1
+    fi
+
+    echo "GPG validation of git history succeeded."
+}
+
 RETURN_VAL=0
+
+# Validate the history
+validate_gpg_gitlog || exit $?
 
 # Remove broken hidden symlinks in $INSTALL_DIR and install home dotfiles
 echo "Installing dotfiles in $INSTALL_DIR"
