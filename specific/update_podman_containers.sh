81da4e5f1c86ddc602afab181802e3337cb8a921
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2021-05-28 20:15:16 +0200

    specific/update_podman_containers: easily update all custom containers

---
+++ specific/update_podman_containers.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2015-2021 Nicolas Iooss
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Update all podman containers which are used in bin/podman-...
+
+# Compute the path to the programs from home-files
+HOME_FILES_BIN="$(realpath "$(dirname -- "$0")/../bin")"
+
+if [ -z "$HOME_FILES_BIN" ] || ! [ -d "$HOME_FILES_BIN" ] ; then
+    echo >&2 "Unable to find home-files/bin directory (${HOME_FILES_BIN})"
+    exit 1
+fi
+echo "Using programs from ${HOME_FILES_BIN}"
+
+set -eux
+
+if podman image exists localhost/podman-bpytop ; then
+    "${HOME_FILES_BIN}/podman-bpytop" --update --no-start
+fi
+if podman image exists localhost/podman-codespell ; then
+    "${HOME_FILES_BIN}/podman-codespell" --update /dev/null
+fi
+if podman image exists localhost/podman-markdownlint ; then
+    "${HOME_FILES_BIN}/podman-markdownlint" --update /dev/null
+fi
+
+# After these updates, update all the other standard images
+"${HOME_FILES_BIN}/podman-update"
